f73a9a3216b212f83e59f847a5d2fa7bb37e65d4
ChenguK <ChenguKargbo@gmail.com>
2020-07-10 17:57:28 -0400

    adds list data to DOM

--- js/script.js
+++ js/script.js
@@ -17,14 +17,32 @@ function handleClick(evt) {
 }
 getPokemon(); // makes the data available as soon as the pagel loads
 
-function getPokemon() { //ajax(baseURL) is a promise. When it runs, 'then' runs
+function getPokemon(evt) { //ajax(baseURL) is a promise. When it runs, 'then' runs
     $.ajax(baseURL) // one line broken up for organization. no semi-colon.
     .then(
         function(data){
             pokemon = data.results;
+            render(); // having the render here means it renders programmatically immediately.
             // console.log("Data: ", data);
     }, function(error) {
             console.log("Error: ", error)
     });
+}
 
+// p represent every pokemon in the pokemon array.
+
+function generateHTml() { 
+    return pokemon.map(function(p) {
+        return `
+        <li class="collection-item red-text">
+            <div style="text-transform: capitalize;">${p.name}<span data-url="${p.url}" class="secondary-content blue-text">
+                    Detail</span>
+            </div>
+        </li>`
+    })
+}
+
+function render() {
+    const html = generateHTml().join("");
+    $ulEL.html(html);
 }
